6a18480a7a84212b4279a04e317726e40275c324
Mariya RAKHMAN <mrakhman@student.42.fr>
2019-06-29 20:40:17 +0200

    study

---
+++ front/src/components/counter.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    state = {
+      count: 0,
+        imgUrl: 'https://picsum.photos/200'
+    };
+    render() {
+        return (
+                <div>
+                    {/*<img src={this.state.imgUrl} alt=""/>*/}
+                    <span className="badge badge-primary m-2">{this.formatCount()}</span>
+                    <button className="btn btn-secondary btn-sm">Increment</button>
+                </div>
+            );
+    }
+
+    formatCount() {
+        const {count} = this.state;
+        return count === 0 ? <h1>Zero</h1> : count;
+    }
+}
+
+export default Counter;
