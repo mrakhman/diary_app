3c69c1a01209779a881619b5491b0c9836c2c866
Mariya RAKHMAN <mrakhman@student.42.fr>
2019-06-29 20:58:25 +0200

    study

--- front/src/components/counter.jsx
+++ front/src/components/counter.jsx
@@ -3,14 +3,24 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state = {
       count: 0,
-        imgUrl: 'https://picsum.photos/200'
+        imgUrl: 'https://picsum.photos/200',
+        tags: ['tag1', 'tag2', 'tag3'],
+        tags2: []
     };
+    renderTags() {
+        if (this.state.tags2.length === 0)
+            return <p> No tags! </p>;
+        return <ul>
+            {this.state.tags2.map(tag => <li key={tag}>{tag}</li>)}
+        </ul>
+    }
     render() {
         return (
                 <div>
                     {/*<img src={this.state.imgUrl} alt=""/>*/}
-                    <span className="badge badge-primary m-2">{this.formatCount()}</span>
-                    <button className="btn btn-secondary btn-sm">Increment</button>
+                    {/*<span className="badge badge-primary m-2">{this.formatCount()}</span>*/}
+                    {/*<button className="btn btn-secondary btn-sm">Increment</button>*/}
+                    {this.renderTags()}
                 </div>
             );
     }
