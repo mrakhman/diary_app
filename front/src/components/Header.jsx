41fd81b4922023f3feb01c272406e5dc275eeb2c
Mariya RAKHMAN <mrakhman@student.42.fr>
2019-07-26 11:49:14 +0200

    move logo to src and import at Header

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link, NavLink } from "react-router-dom";
-// src={"../../public/diary.png"}
+import Logo from '../diary.png';
 
 class Header extends React.Component {
     render () {
@@ -8,7 +8,7 @@ class Header extends React.Component {
             <div className='header'>
                 <nav className="menu">
                     <Link className="" to="/">
-                        <img src={process.env.PUBLIC_URL + "diary.png"} alt="logo" width="40"/>
+                        <img src={Logo} alt="logo" width="40"/>
                     </Link>
                     <ul className="no-decoration">
                         <li className="no-decoration"><NavLink className="nav-menu" to="/new_post">New post</NavLink></li>
